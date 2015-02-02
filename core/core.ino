3783284f3eebae62a5fa10811413fca5c5dd9384
Logan Walker <logan.walker@me.com>
2015-02-02 00:26:44 -0500

    Minor Edits

--- core/core.ino
+++ core/core.ino
@@ -10,8 +10,8 @@
 #define ledPin 13 //Pin for output LED
 #define analogInPos 0 //Pin for positive half of analog input
 #define analogInNeg 1 //Pin for negitive half of analog input
-#define digitalOutPos 5 //Pin for positive half of digital output (PWM)
-#define digitalOutNeg 6 //Pin for negitive half of digital output (PWM)
+#define digitalOutPos 11 //Pin for positive half of digital output (PWM)
+#define digitalOutNeg 5 //Pin for negitive half of digital output (PWM)
 
 int inPos = 0;
 int inNeg = 0; 
@@ -21,23 +21,29 @@ int outNeg = 0;
 void setup() {
   // declare the ledPin as an OUTPUT
   pinMode(ledPin, OUTPUT);  
+  pinMode(2, INPUT);
   digitalWrite(ledPin, HIGH);
   Serial.begin(9600);
   analogReference(INTERNAL);
   delay(1000);
   digitalWrite(ledPin, LOW);
+  analogReference(INTERNAL);
   Serial.println("Code is setup!");  
 }
 
 void loop() {
-  inPos = analogRead(analogInPos);
-  inNeg = analogRead(analogInNeg);
+  if(digitalRead(2) == HIGH){
+    //Serial.println(outPos);
+    inPos = analogRead(analogInPos);
+    inNeg = analogRead(analogInNeg);
 
-  //Add Sound Edit Code Here
-  outPos = inPos;
-  outNeg = inNeg;
+    //Add Sound Edit Code Here
+    outPos = inPos;
+    outNeg = inNeg;
 
-  analogWrite(digitalOutPos, outPos / 4);
-  analogWrite(digitalOutNeg, outNeg / 4);
+    analogWrite(digitalOutPos, outPos / 4);
+    analogWrite(digitalOutNeg, outNeg / 4);
+  }
 }
+
 
