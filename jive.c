ecee399a71cfb5007fba72a66c650faf19a761b0
Logan Walker <logan.walker@me.com>
2015-05-27 22:08:24 -0500

    Fixed broken merge

--- jive.c
+++ jive.c
@@ -6,11 +6,16 @@
 #include <math.h>
 #include <time.h>
 
+#define BITHEIGHT 32765
 #define FREQUENCY 44000
 
 union BufferType buffer;
+long long int sampletotal;
+int samples;
+double timeDiff;
 
 union BufferType {
+	signed short int input;
 	char parts[2];
 };
 
@@ -19,9 +24,13 @@ void writeBuffer(union BufferType piece) {
 }
 
 void delayNanoSecs(long int seconds, long int nanoseconds) {
+	struct timespec time, time2;
+	time.tv_sec = seconds;
+	time.tv_nsec = nanoseconds;
+	nanosleep(&time , &time2);
 }
 
-
+int main(int argCount, char ** args) {
 	fprintf(stderr, "Starting JiveBox 0.1 Terminal...\n");
 
 	buffer.input = 0;
@@ -32,13 +41,33 @@ void delayNanoSecs(long int seconds, long int nanoseconds) {
 	fprintf(stderr, "\tLocation of part #2: %p\n\n", &buffer.parts[1]);
 
 	fprintf(stderr, "Initial Variable States:\n");
+	fprintf(stderr, "\tBuffer input: %d\n", buffer.input);
 	fprintf(stderr, "\tBuffer part #1: %c\n", buffer.parts[0]);
 	fprintf(stderr, "\tBuffer part #2: %c\n\n", buffer.parts[1]);
 
 	timeDiff = 1.0 / FREQUENCY;
+	double timeDiff_uSeconds = timeDiff * 1000000;
+
+	for(unsigned short int wavefreq = 200; wavefreq < 1000; wavefreq += 100) {
+
+		sampletotal = 0;
+		samples = 0;
+
+		while((sampletotal + samples) < FREQUENCY * 3) {
+			writeBuffer(buffer);
+
+			double fofx = sin(TWOPI * wavefreq * timeDiff * (samples + sampletotal));
+			fofx *= BITHEIGHT;
+			fofx = floor(fofx);
+			buffer.input = (signed int) fofx;
 
 			samples++;
+			if(samples == FREQUENCY) {
+				sampletotal += samples;
+				samples = 0;
+			}
 		}
+
 	}
 
 	return 0;
