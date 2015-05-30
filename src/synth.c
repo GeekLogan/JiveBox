bbbf416654380e26189ae4692b1aeff1e98a11da
Logan Walker <logan.walker@me.com>
2015-05-30 11:15:21 -0500

    Finish refactoring synths and add minor comments

--- src/synth.c
+++ src/synth.c
@@ -28,16 +28,16 @@ double fm_synth(double t, int wavefreq1, int wavefreq2) {
 }
 
 double sawtooth_synth(double t, int wavefreq) {
-    double x = t * wavefreq;
-    x = (x - floor(0.5 + x));
-    x = gain_filter_lossless(x, 2.0);
-    return x;
+    double out = t * wavefreq;
+    out = (out - floor(0.5 + out));
+    out = gain_filter_lossless(out, 2.0);
+    return out;
 }
 
 double triangle_synth(double t, int wavefreq) {
     double saw = sawtooth_synth(t, wavefreq);
     if(saw < 0.0) saw = gain_filter_lossless(saw, -1.0);
     saw = gain_filter_lossless(saw, 2.0);
-    saw -= 1.0;
+    saw -= 1.0; //shift range from [0, 2] to [-1, 1]
     return saw;
 }
