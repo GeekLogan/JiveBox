15ca05ba7892e04f134cb75acd76b3884bb32ebd
Logan Walker <logan.walker@me.com>
2015-07-22 19:10:57 -0500

    Reorganize and Delete Old Code

--- Depreciated C Code/src/filters.h
+++
@@ -1,5 +0,0 @@
-signed short lowpass_filter(signed short val, signed short filter);
-signed short highpass_filter(signed short val, signed short filter);
-signed short gain_filter(signed short val, double rate);
-double gain_filter_lossless(double val, double rate);
-signed short bitcrush_filter(signed short val, double rate);
